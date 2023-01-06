937ca9b6857f7826a77d2a62f2b23884e11efa05
WizardGoose <21jturgoose@gmail.com>
2023-01-06 14:52:45 -0700

    Grade Calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        float percentage = float.Parse(answer);
+        Console.WriteLine();
+
+        string letter = "";
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 87)
+        {
+            letter = "A-";
+        }
+        else if (percentage >= 83)
+        {
+            letter = "B+";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 77)
+        {
+            letter = "B-";
+        }
+        else if (percentage >= 73)
+        {
+            letter = "C+";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 67)
+        {
+            letter = "C-";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (percentage < 60)
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is: {letter}");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class.  Keep improving and you'll get it next time!");
+        }
+
     }
 }
