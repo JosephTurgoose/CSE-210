444f33f8a56663af23cc47eff60ceb29116f8203
WizardGoose <21jturgoose@gmail.com>
2023-01-06 14:22:44 -0700

    Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask for name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine();
+        // Convert first character to uppercase
+        firstName = (char.ToUpper(firstName[0]) + firstName.Substring(1)).ToString();
+        lastName = (char.ToUpper(lastName[0]) + lastName.Substring(1)).ToString();
+        // Print name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
